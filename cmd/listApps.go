2cb9082fdd36f8f118d9550f3b21dfa6369990c5
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-08-31 17:48:26 -0400

    update

--- cmd/listApps.go
+++ cmd/listApps.go
@@ -16,9 +16,8 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/tgs266/neon-cli/internal/neon"
 )
 
 // listAppsCmd represents the listApps command
@@ -32,20 +31,11 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("listApps called")
+		host, _ := cmd.Flags().GetString("host")
+		neon.ListApps(host)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listAppsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listAppsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listAppsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
