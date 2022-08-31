0b3b0305cee0b7a2f91ed67dcd69f7d14389b3f0
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-08-31 17:47:21 -0400

    add command

--- internal/neon/app.go
+++ internal/neon/app.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -16,10 +17,20 @@ func CreateApp(host string, manifest api.AppManifest) {
 		log.Println("ERROR: ", err)
 		return
 	}
-	_, err = http.Post(fmt.Sprintf("%s/api/v1/apps", host), "application/json", bytes.NewBuffer(manifestJSON))
-	if err != nil {
-		log.Println("ERROR: ", err)
+	resp, err := http.Post(fmt.Sprintf("%s/api/v1/apps", host), "application/json", bytes.NewBuffer(manifestJSON))
+	if err != nil || resp.StatusCode >= 400 {
+		log.Printf("ERROR: Failed to create app %s\n", manifest.Name)
 		return
 	}
 	fmt.Printf("Successfully created app %s\n", manifest.Name)
+}
+
+func ListApps(host string) {
+	resp, err := http.Get(fmt.Sprintf("%s/api/v1/apps", host))
+	if err != nil || resp.StatusCode >= 400 {
+		log.Println("ERROR: could not get apps")
+		return
+	}
+	str, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(str))
 }
